2a1ae8b593fd32e79b838306dbc837b43d71fc67
Marija Jeremic <maja.jeremic31@gmail.com>
2019-03-16 16:42:44 +0100

    Updates

---
+++ precache-manifest.dd221daad1849fd27a76f715b2710807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d8737e209a192981209",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "1d8737e209a192981209",
+    "url": "/contactmanager/static/js/main.1d8737e2.chunk.js"
+  },
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "49acc6acd4863a540f23",
+    "url": "/contactmanager/static/css/2.e817d8ac.chunk.css"
+  },
+  {
+    "revision": "49acc6acd4863a540f23",
+    "url": "/contactmanager/static/js/2.49acc6ac.chunk.js"
+  },
+  {
+    "revision": "9428276c4dc2186fb8ff285cdcc17c1b",
+    "url": "/contactmanager/index.html"
+  }
+];
